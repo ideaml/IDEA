c18a55b3fc1ccf0c216df0152764d562892d60ea
ideaml <37802176+ideaml@users.noreply.github.com>
2018-05-30 22:59:16 +0800

    Update every.js

--- every.js
+++ every.js
@@ -19,3 +19,40 @@ let obj = (mappingItemId) ? {itemId,mappingItemId,totalItemId} : null;
 
 console.log(obj);
 console.timeEnd('split');
+
+
+
+
+
+let changevalues = {
+    existLifeAXA:1,
+    existLifeOther:2,
+    existLife:3,
+    timTDCAXA:'qwe',
+    timTDCOther:3,
+    timTDC:'r'
+};
+let AXA = ['existLifeAXA','existTDCAXA','existLMPAXA','timLifeAXA','timTDCAXA','timLMPAXA'];
+let Other = ['existLifeOther','existTDCOther','existLMPOther','timLifeOther','timTDCOther','timLMPOther'];
+let Total = ['existLife','existTDC','existLMP','timLife','timTDC','timLMP'];
+console.log(changevalues);
+console.time('isValid');
+
+let size = AXA.length;
+let isValid = true;
+for (let i = 0; i < size; i++) {
+    let keyAXA = AXA[i], keyOther = Other[i], keyTotal = Total[i];
+    if(changevalues.hasOwnProperty(keyAXA) &&  changevalues.hasOwnProperty(keyOther) && changevalues.hasOwnProperty(keyTotal)){
+        let valueAXA = Number(changevalues[keyAXA]);
+        let valueOther = Number(changevalues[keyOther]);
+        let valueTotal = Number(changevalues[keyTotal]); 
+        console.log(valueAXA + valueOther);
+        if(valueAXA + valueOther != valueTotal){
+            isValid = false;
+            break;
+        }
+    }
+}
+console.log(isValid);
+
+console.timeEnd('isValid');
